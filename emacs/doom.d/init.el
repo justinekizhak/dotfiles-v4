0f9ae1f67f6492877a98ce938acad14891eb6963
Justine Kizhakkinedath <justine@kizhak.com>
2020-01-04 16:08:18 +0530

    Update init.el

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -170,7 +170,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
