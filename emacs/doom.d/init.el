cf823f34c02b47cda9d0255145ecea3872a8a78b
Justine Kizhakkinedath <justine@kizhak.com>
2020-02-15 11:48:50 +0530

    Fix flycheck, flyspell init module error

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -75,6 +75,10 @@
        ;;term              ; terminals in Emacs
        vterm             ; another terminals in Emacs
 
+       :checkers
+       syntax            ; tasing you for every semicolon you forget
+       spell             ; tasing you for misspelling mispelling
+
        :tools
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
@@ -83,8 +87,6 @@
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       flycheck          ; tasing you for every semicolon you forget
-       flyspell          ; tasing you for misspelling mispelling
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +devdocs
@@ -174,7 +176,7 @@
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       write             ; emacs for writers (fiction, notes, papers, etc.)
+       ;; write             ; emacs for writers (fiction, notes, papers, etc.)
 
        :config
        literate
