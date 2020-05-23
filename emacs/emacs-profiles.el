b2531a8178ff2162c60eb371ffa2344531cbca2c
Mukesh Kumar <mukesh.kumar@mindstix.com>
2020-05-23 11:48:06 +0530

    Add chemas config

---
+++ emacs/emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default"   . ((user-emacs-directory . "~/doom-emacs")))
+    ("doom"   . ((user-emacs-directory . "~/doom-emacs")))
+    ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
+    ("prelude"   . ((user-emacs-directory . "~/prelude"))))
