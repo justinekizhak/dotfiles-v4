af4c64499c16258979ca459b02e1075e891d486d
Justine Kizhakkinedath <justine@kizhak.com>
2020-05-30 16:16:33 +0530

    Update emacs profiles

--- emacs/emacs-profiles.el
+++ emacs/emacs-profiles.el
@@ -1,4 +1,4 @@
 (("default"   . ((user-emacs-directory . "~/doom-emacs")))
-    ("doom"   . ((user-emacs-directory . "~/doom-emacs")))
-    ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
-    ("prelude"   . ((user-emacs-directory . "~/prelude"))))
+ ("doom"   . ((user-emacs-directory . "~/doom-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
+ ("prelude"   . ((user-emacs-directory . "~/prelude"))))
